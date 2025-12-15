bd1c590fc7b78707fe07759212eb29a7a62939d9
Mason Speck <mcs.5299@gmail.com>
2025-12-14 18:56:16 -0600

    Update affinity doc

--- include/daedalus/affinity.h
+++ include/daedalus/affinity.h
@@ -16,18 +16,21 @@ namespace affinity
  * This is an abstraction over native APIs for Windows and Linux. If you don't have a std::thread and only have a
  * native handle, then instead copy the implementation of this function for the required platform.
  *
- * The term 'logical processor' is derived from the MSDN definition
- * https://learn.microsoft.com/en-us/windows/win32/procthread/processor-groups
- * and refers not to physical CPU cores but to
+ * The term 'logical processor' is derived from the MSDN definition here:
+ * https://learn.microsoft.com/en-us/windows/win32/procthread/processor-groups and refers to logical processors exposed
+ * by the CPU. This refers primarily to cases like hyperthreading, where the Operating System exposes a single
+ * hyperthreaded cpu core as two separate logical processors.
  *
- * Currently on Windows this function will only work for logical processor counts of 64 or less. Windows uses a concept
- * of processor groups, each of which can contain only 64 options. This implementation ignores that detail and just
- * assumes the processor can have at most 64 logical processors.
+ * Currently on Windows this function will allow setting logical processor affinities of id 64 or less. Windows uses a
+ * concept of processor groups, each of which can contain only 64 options, and referring to logical processors beyond
+ * the default processor group requires changing the processor group of the thread. This implementation ignores that
+ * detail and just assumes the processor can have at most 64 logical processors.
  *
  * https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-setthreadaffinitymask
  *
  * @param thread The std::thread to attempt to set logical processor affinity on.
- * @param target_logical_processor_ids A 0-indexed span of logical processors to set affinity to.
+ * @param target_logical_processor_ids A 0-indexed span of logical processors to set affinity to. On Windows, expects a
+ * given id to be in the range [0, 63].
  *
  * @return True if setting affinity was reported successful by the Operating System (this does not necessarily guarantee
  * expected behavior).
